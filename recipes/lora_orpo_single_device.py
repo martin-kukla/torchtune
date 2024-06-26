ORPO: Fixing mean computation for logps + logging nll_loss_chosen

recipes/lora_orpo_single_device.py
@@ -465,7 +465,7 @@ class LoRAORPORecipeSingleDevice(FTRecipeInterface):
             logits.log_softmax(-1), dim=2, index=labels.unsqueeze(2)
         ).squeeze(2)
 
-        return torch.nanmean(per_token_log_probs * loss_mask, dim=-1)
+        return (per_token_log_probs * loss_mask).sum(-1) / loss_mask.sum(-1)
 
     def train(self) -> None:
         """
@@ -500,7 +500,7 @@ class LoRAORPORecipeSingleDevice(FTRecipeInterface):
                     rejected_logits,
                 ) = self.concatenated_forward(self._model, batch)
 
-                loss, chosen_rewards, rejected_rewards, or_loss, log_odds_chosen, log_odds_rejected = self._loss_fn(
+                loss, chosen_rewards, rejected_rewards, nll_loss_chosen, or_loss, log_odds_chosen, log_odds_rejected = self._loss_fn(
                     avg_chosen_log_probs,
                     avg_rejected_log_probs,
                 )
@@ -548,6 +548,7 @@ class LoRAORPORecipeSingleDevice(FTRecipeInterface):
                             .mean()
                             .cpu(),
                             "logits/chosen": chosen_logits.detach().mean().cpu(),
+                            "orpo/nll_loss_chosen": nll_loss_chosen.detach().mean().cpu(),
                             "orpo/or_loss": or_loss.detach().mean().cpu(),
                             "orpo/log_odds_chosen": log_odds_chosen.detach().mean().cpu(),
                             "orpo/log_odds_rejected": log_odds_rejected.detach().mean().cpu(),

torchtune/modules/loss/orpo.py
@@ -50,6 +50,7 @@ class ORPOLoss(nn.Module):
                 - losses: The ORPO loss for each example in the batch.
                 - chosen_rewards: Rewards for the chosen responses.
                 - rejected_rewards: Rewards for the rejected responses.
+                - nll_loss_chosen: NLL loss for chosen response.
                 - or_loss: Odd ratio loss.
                 - log_odds_chosen: Log odds chosen.
                 - log_odds_rejected: Log odds rejected.
@@ -61,7 +62,8 @@ class ORPOLoss(nn.Module):
         log_odds_chosen = avg_chosen_logps - torch.log1p(-torch.exp(avg_chosen_logps))
         log_odds_rejected = avg_rejected_logps - torch.log1p(-torch.exp(avg_rejected_logps))
         or_loss = - F.logsigmoid(log_odds_chosen - log_odds_rejected)
-        losses = - avg_chosen_logps  + self.beta * or_loss
+        nll_loss_chosen = - avg_chosen_logps # Other implementations make extra call to CrossEntropy/NLLLoss pytorch layer, which I don't think we need?
+        losses =  nll_loss_chosen + self.beta * or_loss
 
         chosen_rewards = (
             avg_chosen_logps.detach()
@@ -70,4 +72,4 @@ class ORPOLoss(nn.Module):
             avg_rejected_logps.detach()
         )
 
-        return losses, chosen_rewards, rejected_rewards, or_loss, log_odds_chosen, log_odds_rejected
+        return losses, chosen_rewards, rejected_rewards, nll_loss_chosen, or_loss, log_odds_chosen, log_odds_rejected